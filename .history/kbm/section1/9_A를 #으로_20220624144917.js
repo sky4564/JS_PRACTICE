6467b6ba457a39e245a765c8cd72eddc8e86264b
sky4564 <qw486512@gmail.com>
2022-06-24 15:55:46 +0900

    remove history

--- .history/kbm/section1/9_A를 #으로_20220624144917.js
+++
@@ -1,7 +0,0 @@
-            function solution(s){
-                let answer = s    
-                return 'asd'
-            
-            }
-            let str="BANANA";
-            console.log(solution(str));
