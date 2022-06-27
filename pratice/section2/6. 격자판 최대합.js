51b53fe646be29befb1b94734c1cc38a5882935f
sky4564 <qw486512@gmail.com>
2022-06-27 16:52:25 +0900

    pratice update2

--- pratice/section2/6. 격자판 최대합.js
+++ pratice/section2/6. 격자판 최대합.js
@@ -1,22 +1,5 @@
             function solution(arr){  
-                let answer=Number.MIN_SAFE_INTEGER;
-                let n=arr.length;
-                let sum1=sum2=0;
-                for(let i=0; i<n; i++){
-                    sum1=sum2=0;
-                    for(let j=0; j<n; j++){
-                        sum1+=arr[i][j];
-                        sum2+=arr[j][i];
-                    }
-                    answer=Math.max(answer, sum1, sum2);
-                }
-                sum1=sum2=0;
-                for(let i=0; i<n; i++){
-                    sum1+=arr[i][i];
-                    sum2+=arr[i][n-i-1];
-                }  
-                answer=Math.max(answer, sum1, sum2); 
-                return answer;
+
             }
 
             let arr=[[10, 13, 10, 12, 15], 
--- pratice/section2/7. 봉우리.js
+++ pratice/section2/7. 봉우리.js
@@ -1,24 +1,4 @@
             function solution(arr){  
-                let answer=0;
-                let n=arr.length;
-                let dx=[-1, 0, 1, 0];
-                let dy=[0, 1, 0, -1];
-                for(let i=0; i<n; i++){
-                    for(let j=0; j<n; j++){
-                        let flag=1;
-                        for(let k=0; k<4; k++){
-                            let nx=i+dx[k];
-                            let ny=j+dy[k];
-                            if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]>=arr[i][j]){
-                                flag=0;
-                                break;
-                            }
-                        }
-                        if(flag) answer++;
-                    }
-                }  
-                  
-                return answer;
             }
 
             let arr=[[5, 3, 7, 2, 3], 
