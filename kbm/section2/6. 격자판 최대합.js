f74350b9237f713b4776655d9f5418329a3de0d7
sky4564 <qw486512@gmail.com>
2022-06-29 16:18:35 +0900

    구조변경 및 문제풀이 update

--- kbm/section2/6. 격자판 최대합.js
+++ kbm/section2/6. 격자판 최대합.js
@@ -1,5 +1,45 @@
-            function solution(arr){  
+            function solution(arr){
+                let answer = 0
+                let sum1 = 0
+                let sum2 = 0
+                let n = arr.length
+                let Lsum = 0
+                let Rsum = 0
+                let show =[]
+                for(let i = 0; i < arr.length; i++){
+
+                    for(let j =0 ; j < arr[i].length; j++){
+                        sum1  += arr[i][j]
+                        sum2 += arr[j][i]
+                    }
+                    show.push(sum1)
+                    show.push(sum2)                    
+                    sum1 = 0
+                    sum2 = 0
+                    answer = Math.max(answer,sum1, sum2)
+                }
+                for(let i = 0; i < arr.length; i++){
+                            Rsum += arr[i][i]
+                            Lsum += arr[i][n-1-i]                
+                }
+                answer = Math.max(answer,Rsum, Lsum)
+                show.push(Lsum)
+                show.push(Rsum)
+
+                show.sort(function (a,b){
+                    return a-b
+                })
+                
+                // console.log(show)
+                // show.sort 사용해서
+                // answer = show[show.length-1]
+                
+                // 선생님은 Math.max 활용함
+
+                
+                return answer
             }
+            
 
             let arr=[[10, 13, 10, 12, 15], 
                      [12, 39, 30, 23, 11],
