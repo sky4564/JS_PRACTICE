99d83e1804e6e6ad10c9626acb87e25a145b48f3
sky4564 <qw486512@gmail.com>
2022-06-29 16:35:14 +0900

    update

---
+++ common/pratice/section3/2. 유효한 팰린드롬.js
@@ -0,0 +1,5 @@
+function solution(s){
+}
+
+let str="found7, time: study; Yduts; emit, 7Dnuof";
+console.log(solution(str));
---
+++ common/pratice/section3/3. 숫자만 추출.js
@@ -0,0 +1,5 @@
+function solution(str){
+}
+
+let str="g0en2T0s8eSoft";
+console.log(solution(str));
