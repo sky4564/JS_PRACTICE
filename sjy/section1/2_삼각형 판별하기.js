57914291332e186e7dabf9111b0e276dc09add3a
sjy <jen.jyseo@gmail.com>
2022-07-01 17:06:02 +0900

    commit 1

--- sjy/section1/2_삼각형 판별하기.js
+++ sjy/section1/2_삼각형 판별하기.js
@@ -3,11 +3,11 @@ function solution(a, b, c) {
   const longest = (a > b ? a : b) > c ? (a > b ? a : b) : c;
 
   // 나머지 두 막대 길이의 합 구하기
-  const rest = a + b + c - longestBar;
+  const restBar = a + b + c - longestBar;
 
   // 나머지 두 막대 길이의 합 보다 제일 큰 막대 길이가 짧을 경우
   // return -> "YES" / 아닐 경우 -> "NO"
-  const answer = rest > longest ? "YES" : "NO";
+  const answer = restBar > longest ? "YES" : "NO";
 
   return answer;
 }
--- sjy/section1/5_최솟값 구하기.js
+++ sjy/section1/5_최솟값 구하기.js
@@ -1,6 +1,13 @@
-            function solution(arr){         
+/** 인잘 전달된 값이 배열인 경우 */
 
-            }
+function solution(array) {
+  // sort에 오름차순으로 배열을 정렬하는 콜백 전달
+  // retrun -> 오름차순으로 정렬된 rest 배열
+  // 0번째 인덱스에 해당하는 가장 작은 수를 answer에 대입
 
-            let arr=[5, 7, 1, 3, 2, 9, 11];
-            console.log(solution(arr));
+  const [answer] = array.sort((a, b) => a - b);
+
+  return answer;
+}
+
+console.log(solution([5, 3, 7, 11, 2, 15, 17]));
