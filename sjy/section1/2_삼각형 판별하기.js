4eefd5d7534180e670c05dbf037b3ced8f2a5253
sjy <jen.jyseo@gmail.com>
2022-07-01 14:26:42 +0900

    commit 7

--- sjy/section1/2_삼각형 판별하기.js
+++ sjy/section1/2_삼각형 판별하기.js
@@ -7,9 +7,9 @@ function solution(a, b, c) {
 
   // 나머지 두 막대 길이의 합 보다 제일 큰 막대 길이가 짧을 경우
   // return -> "YES" / 아닐 경우 -> "NO"
-  const solution = rest > longest ? "YES" : "NO";
+  const answer = rest > longest ? "YES" : "NO";
 
-  return solution;
+  return answer;
 }
 
 console.log(solution(13, 33, 17));
