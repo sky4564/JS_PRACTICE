35ecb04ebff6b92755817f1177caa98fcf3abf3b
sky4564 <qw486512@gmail.com>
2022-07-01 17:57:43 +0900

    update

--- kbm/section4/1. 자리수의 합.js
+++ kbm/section4/1. 자리수의 합.js
@@ -1,7 +1,42 @@
 function solution(n, arr){
+    let ans
+    let max = Number.MIN_SAFE_INTEGER
+    for(let x of arr){
+        let tmp = x        
+        let sum = 0
+        while(tmp){
+            sum +=(tmp%10)
+            tmp = Math.floor(tmp/10)
+        }        
+        if(sum > max){
+            max = sum
+            ans = x            
+        }else if(sum === max){
+            if(ans<x) ans = x
+        }
+    }
+    return ans
+}
+
+function solution2(n, arr){
+    let ans
+    let max = Number.MIN_SAFE_INTEGER
+    for(let x of arr){
+        let sum =x.toString().split('').reduce((a,b)=> a+Number(b), 0);
+        console.log(sum)
+        if(sum > max){
+            max = sum
+            ans = x            
+        }else if(sum === max){
+            if(ans<x) ans = x
+        }
+    }
+    return ans
 }
 
 
-let arr=[128, 460, 603, 40, 521, 137, 123];
+
+let arr=[1244, 460, 603, 40, 521, 137, 123];
 console.log(solution(7, arr));
+console.log(solution2(7, arr));
         
--- kbm/section4/2. 뒤집은 소수.js
+++ kbm/section4/2. 뒤집은 소수.js
@@ -1,14 +1,51 @@
 function isPrime(num){
-    if(num===1) return false;
-    for(let i=2; i<=parseInt(Math.sqrt(num)); i++){
-        if(num%i===0) return false;
+    for(let i = 2 ; i < 10; i++){
+        if(num % i === 0) return false
     }
-    return true;
+    return true
 }
 function solution(arr){
+    let tmp
+        
+    for(let i =0 ; i < arr.length; i++){
+            tmp = arr.toString().split(',').reverse()
+            
+        
+        for(let j = 0; j <tmp[i].length; j++){
+            console.log(tmp[i][j])
+            let block = tmp[i][j]
+            tmp[i][j] = tmp[i][tmp[i].length-1-j]
+            tmp[i][tmp[i].length-1-j] = block
+        }
+    }
+    console.log(tmp)
+}
+
+function isPrime2(num){
+    for(let i = 2 ; i < 10; i++){
+        if(num % i === 0) return false
+    }
+    return true
+}
+function solution2(arr){
+    let ans =[]
+    for(let x of arr){
+        let res =0;
+        while(x){
+            let t = x%10    
+            res  = res*10 + t
+            x =  x/10
+        }
+        ans.push(res)
+    }
+    return ans        
 }
 
             
 let arr=[32, 55, 62, 20, 250, 370, 200, 30, 100];
-console.log(solution(arr));
+// console.log(solution(arr))
+console.log(solution2(arr))
+// console.log(arr)
+// console.log(arr.toString())
+// console.log(arr.toString().split(','))
             
