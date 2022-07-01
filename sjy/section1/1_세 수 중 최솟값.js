77cb992ac5e8641fe82b348a0c010bd36216bc76
sjy <jen.jyseo@gmail.com>
2022-07-01 14:24:45 +0900

    commit 6

--- sjy/section1/1_세 수 중 최솟값.js
+++ sjy/section1/1_세 수 중 최솟값.js
@@ -1,7 +1,7 @@
 function solution(a, b, c) {
   // a, b 비교
   // a, b 중 더 작은 수와 c 비교
-  // c 보다 a나 b가 작다면 a, b 중 더 큰 수 다음으로 c를 정렬
+  // c 보다 a나 b가 작다면 a, b 중 더 작은 수를 값으로 평가
   const answer = (a < b ? a : b) < c ? (a < b ? a : b) : c;
 
   return answer;
