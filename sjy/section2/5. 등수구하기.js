f8963dc024dd162ddfefcae8358c771fb61436b1
sjy <jen.jyseo@gmail.com>
2022-07-12 17:32:33 +0900

    commit 1

--- sjy/section2/5. 등수구하기.js
+++ sjy/section2/5. 등수구하기.js
@@ -1,5 +1,11 @@
-            function solution(arr){  
-            }
-
-            let arr=[87, 89, 92, 100, 76];
-            console.log(solution(arr));
+function solution(arr) {
+  let answer = arr.map((item) => 1);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] > arr[j]) answer[i] += 1;
+    }
+  }
+  return answer.join("");
+}
+let arr = [92, 80, 72, 100, 76];
+console.log(solution(arr));
