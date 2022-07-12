384db19d5d968c85356f5f462401586a974251a6
sjy <jen.jyseo@gmail.com>
2022-07-12 16:38:52 +0900

    commit 1

--- sjy/section2/3. 가위바위보.js
+++ sjy/section2/3. 가위바위보.js
@@ -2,15 +2,25 @@ function solution(a, b) {
   let answer = "";
   for (let i = 0; i < a.length; i++) {
     answer +=
+      // a와 b가 동일하다면 비긴것 -> D
       a[i] === b[i]
         ? "D"
-        : a[i] > b[i]
-        ? a[i] - b[i] === 2
+        : // a와 b가 동일하지 않다면 비교
+        a[i] > b[i]
+        ? // a가 b보다 크다면
+          // a에서 b를 뺐을 때의 값이 2라면 b의 승리
+          // (a는 보, b는 가위)
+          a[i] - b[i] === 2
           ? "B"
-          : "A"
-        : b[i] - a[i] === 2
+          : // 2가 아니라면 a의 승리
+            "A"
+        : // b가 a보다 크다면
+        // b에서 a를 뺐을 때의 값이 2라면 b의 승리
+        // (b는 보, a는 가위)
+        b[i] - a[i] === 2
         ? "A"
-        : "B";
+        : // 2가 아니라면 b의 승리
+          "B";
   }
 
   return answer;
--- sjy/section2/4. 점수 계산.js
+++ sjy/section2/4. 점수 계산.js
@@ -1,6 +1,21 @@
-            function solution(arr){         
+function solution(arr) {
+  // 정답
+  let answer = 0;
+  // 점수
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      // 정답이라면 점수에 1추가한다
+      // 추가된 점수를 정답에 더한다
+      count++;
+      answer += count;
+    } else {
+      // 정답이 아니라면 점수를 0으로 초기화한다
+      count = 0;
+    }
+  }
+  return answer;
+}
 
-            }
-
-            let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
-            console.log(solution(arr));
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
