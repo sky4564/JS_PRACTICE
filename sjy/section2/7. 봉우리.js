07239e08ed4d2f2ee964f356ce346746dc62a363
sjy <jen.jyseo@gmail.com>
2022-07-12 20:03:24 +0900

    commit 1

--- sjy/section2/7. 봉우리.js
+++ sjy/section2/7. 봉우리.js
@@ -1,9 +1,13 @@
-            function solution(arr){  
-            }
+function solution(arr) {
+  // arr의 첫 번째, 마지막 배열
+  // 각 배열의 첫 번째, 마지막 인덱스
+}
 
-            let arr=[[5, 3, 7, 2, 3], 
-                     [3, 7, 1, 6, 1],
-                     [7, 2, 5, 3, 4],
-                     [4, 3, 6, 4, 1],
-                     [8, 7, 3, 5, 2]];
-            console.log(solution(arr));
+let arr = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
+console.log(solution(arr));
