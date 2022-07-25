6c15b9f1d1636082152aecb3ad91a4df6f0338a4
sjy <jen.jyseo@gmail.com>
2022-07-25 17:06:36 +0900

    commit 1

--- sjy/section3/3. 숫자만 추출.js
+++ sjy/section3/3. 숫자만 추출.js
@@ -1,5 +1,7 @@
-function solution(str){
+function solution(str) {
+  const answer = str.replace(/[^0-9]/g, "").replace(/^0/g, "");
+  return answer;
 }
 
-let str="g0en2T0s8eSoft";
+let str = "g0en2T0s8eSoft";
 console.log(solution(str));
