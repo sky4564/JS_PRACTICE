3d1df10b5fa9a491adafddd365b5ed3dd678e7c6
sjy <jen.jyseo@gmail.com>
2022-07-25 18:44:27 +0900

    commit 1

--- sjy/section3/4. 가장 짧은 문자거리.js
+++ sjy/section3/4. 가장 짧은 문자거리.js
@@ -1,6 +1,31 @@
-function solution(s, t){
+function solution(s, t) {
+  const answer = [];
+
+  let p = 1000;
+
+  for (let x of s) {
+    if (x === t) {
+      p = 0;
+      answer.push(p);
+    } else {
+      p++;
+      answer.push(p);
+    }
+  }
+  p = 1000;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      p = 0;
+    } else {
+      p++;
+      // Math.min의 첫 번째 인자인 answer[i] ==> t 값이 왼쪽에서부터 떨어져있는 거리
+      // 왼쪽에서부터 t값과 떨어진 거리와 오른쪽에서부터 떨어진 값 중 작은 값을 대입
+      answer[i] = Math.min(answer[i], p);
+    }
+  }
+
+  return answer;
 }
 
-let str="teachermode";
-console.log(solution(str, 'e'));
-        
+let str = "teachermode";
+console.log(solution(str, "e"));
