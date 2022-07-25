a53a41e1a17904ffffb30c2ca87f020447a89b0a
sjy <jen.jyseo@gmail.com>
2022-07-25 16:49:31 +0900

    commit 1

--- sjy/section3/1. 회문문자열.js
+++ sjy/section3/1. 회문문자열.js
@@ -1,3 +1,18 @@
-function solution(s) {
+function solution(input) {
+  const reverseInput = input.split("").reverse().join("");
+
+  const regex = new RegExp(`[${input}]`, "gi");
+
+  const answer = reverseInput.match(regex) ? "YES" : "NO";
+
   return answer;
+
+  /** 선생님 풀이
+   * reverse 사용이 제한될 경우
+   * input을 처음부터 대문자나 소문자로 전부 변환한다.
+   * 변환한 문자열을 반복문을 돌려 문자열의 앞과 뒤의 인덱스를 구해 값이 동일한지 확인.
+   */
 }
+
+const item = "gooG";
+console.log(solution(item));
--- sjy/section3/2. 유효한 팰린드롬.js
+++ sjy/section3/2. 유효한 팰린드롬.js
@@ -1,5 +1,20 @@
-function solution(s){
+function solution(s) {
+  const answer = "YES";
+  const filteredInput = s.toLowerCase().replace(/[^a-z]/g, "");
+  const inputLength = Math.floor(filteredInput.length / 2);
+
+  for (let i = 0; i < inputLength; i++) {
+    if (filteredInput[i] !== filteredInput[filteredInput.length - 1 - i]) {
+      return "NO";
+    }
+  }
+
+  /** reverse를 사용한 풀이 */
+  //   const reverseInput = filteredInput.split("").reverse().join("");
+  //   const answer = reverseInput === filteredInput ? "YES" : "NO";
+
+  return answer;
 }
 
-let str="found7, time: study; Yduts; emit, 7Dnuof";
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
