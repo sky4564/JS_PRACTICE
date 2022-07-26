e0e1436f199ee4b2f0525be204304fc5df080450
sjy <jen.jyseo@gmail.com>
2022-07-26 19:46:50 +0900

    commit 1

--- sjy/section3/5. 문자열 압축.js
+++ sjy/section3/5. 문자열 압축.js
@@ -1,6 +1,21 @@
-function solution(s){
+function solution(s) {
+  let answer = "";
+  let count = 1;
 
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    } else {
+      answer += s[i];
+      if (count > 1) {
+        answer += count;
+        count = 1;
+      }
+    }
+  }
+
+  return answer;
 }
 
-let str="KKHSSSSSSSE";
+let str = "KKHSSSSSSSE";
 console.log(solution(str));
