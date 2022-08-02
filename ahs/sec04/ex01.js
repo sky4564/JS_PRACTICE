85a56dc27d90a94f7cd946efff4f45bd29661724
hongsokan <hongsokan@daum.net>
2022-08-02 20:05:18 +0900

    sec04 update

--- ahs/sec04/ex01.js
+++ ahs/sec04/ex01.js
@@ -1,28 +1,47 @@
 // 자릿수의 합
 // 자릿수의 합이 같은 경우 원래 숫자가 더 큰 숫자가 정답
 
-function solution(input){
+function solution(input) {
+
+
+  /*
+  // 강의 풀이
   let numbers = input;
   let max = Number.MIN_SAFE_INTEGER;
   let result = 0;
-  
+
   numbers.forEach(n => {
     let sum = 0;
     let tmp = n;
     while (tmp > 0) {
       sum += tmp % 10;
-      tmp = Math.floor(tmp/10);
+      tmp = Math.floor(tmp / 10);
     }
     if (sum > max) {
       max = sum;
       result = n;
     }
-    else if (sum === max){
+    else if (sum === max) {
       if (n > result) result = n;
     }
   });
-  
+
   return result;
+  */
+
+  let answer, max = Number.MIN_SAFE_INTEGER;
+  for (let x of input) {
+    let sum = x.toString().split('').reduce((a, b) => a + Number(b), 0); // 11 10 9 4 8 11 6
+    if (sum > max) { // 자릿수의 합이 max보다 큰 경우
+      max = sum;
+      answer = x;
+    }
+    else if (sum === max) { // 자릿수의 합이 max와 같은 경우
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+
 }
 
 let n = 7;
--- ahs/sec04/ex02.js
+++ ahs/sec04/ex02.js
@@ -1,6 +1,30 @@
 // 뒤집은 소수
 
-function solution(input){
+/*
+// 강의 풀이
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+function solution(arr) {
+  let answer = [];
+  for (let x of arr) {
+    let res = 0;
+    while (x) {
+      let t = x % 10;
+      res = res * 10 + t;
+      x = parseInt(x / 10);
+    }
+    if (isPrime(res)) answer.push(res);
+  }
+  return answer;
+}
+*/
+
+function solution(input) {
   let numbers = input;
   let result = numbers.map(n => {
     let reversed = [];
@@ -9,13 +33,13 @@ function solution(input){
       reversed.push(tmp % 10);
       tmp = Math.floor(tmp / 10);
     }
-    return Number(reversed.join(""));
-  }).filter(n => {
-    for(var i = 2; i < n; i++)
-      if(n % i === 0) return false;
-    return n > 1;
+    return Number(reversed.join("")); // 23, 55, 26, 2, 52, 73, 2, 3, 1
   })
-  
+    .filter(n => {
+      for (var i = 2; i < n; i++) { if (n % i === 0) return false; }
+      return n > 1;
+    })
+
   return result;
 }
 
