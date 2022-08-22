188aaae7450ef1b4926dba41a834e417cbca33e6
sky4564 <qw486512@gmail.com>
2022-08-22 09:29:14 +0900

    update

--- kbm/section5/3. 연속부분수열1.js
+++ kbm/section5/3. 연속부분수열1.js
@@ -1,4 +1,17 @@
 function solution(m, arr){
+let ri = li = 0
+let tmp = 0
+let cnt = 0
+for(let ri = 0; ri < arr.length; ri++){
+  tmp += arr[ri]
+  if(tmp === 6) {
+    cnt++
+
+  }
+  while(tmp > 6){
+    tmp -= arr[ri]
+  }
+}
 
 }
 
