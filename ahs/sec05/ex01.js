86b9a4e941bc67409d8ab8d88c687680aa555910
hongsokan <hongsokan@daum.net>
2022-08-29 23:32:42 +0900

    sec05 update

--- ahs/sec05/ex01.js
+++ ahs/sec05/ex01.js
@@ -1,11 +1,31 @@
 // 두 배열 합치기
+// sort 함수는 기본적으로 O(n log n) 의 시간복잡도를 갖고 있음.
+// 투포인터 알고리즘을 통해 이번 문제를 O(n + m)으로 줄일 수 있다.
+// arr1[p1] <= arr2[p2]
 
 function solution(arr1, arr2) {
-    let result = [...arr1, ...arr2].sort((a, b) => a - b);
-    console.log(result);
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = (p2 = 0);
+    while (p1 < n && p2 < m) {
+        if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
+    }
+    // 비교가 끝나고 남은 값 그대로 추가
+    while (p1 < n) answer.push(arr1[p1++]);
+    while (p2 < m) answer.push(arr2[p2++]);
+    return answer;
 }
+
+/*
+function solution(arr1, arr2) {
+    let result = [...arr1, ...arr2].sort((a, b) => a - b);
+    return result;
+}
+*/
 
 let arr1 = [1, 3, 5];
 let arr2 = [2, 3, 6, 7, 9];
 
-solution(arr1, arr2);
+console.log(solution(arr1, arr2));
--- ahs/sec05/ex02.js
+++ ahs/sec05/ex02.js
@@ -1,14 +1,32 @@
 // 공통원소 구하기
 
+function solution(arr1, arr2){
+    let answer = [];
+    arr1.sort((a, b) => a - b); // [1, 2, 3, 5, 9]
+    arr2.sort((a, b) => a - b); // [2, 3, 5, 7, 8]
+    let p1 = (p2 = 0);
+    while (p1 < arr1.length && p2 < arr2.length) {
+        if (arr1[p1] == arr2[p2]) { // 공통원소 찾으면 p1, p2 모두 ++
+            answer.push(arr1[p1++]);
+            p2++; 
+        } 
+        else if (arr1[p1] < arr2[p2]) p1++; // p1 값 작으면 p1++
+        else p2++; // p2 값 작으면 p2++
+    }
+    return answer;
+}
+
+/*
 function solution(arr1, arr2) {
     let result =  arr1
         .filter(element => arr2.includes(element)) 
         .sort((a, b) => a - b);
 
-    console.log(result);
+    return result;
 }
+*/
 
 let arr1 = [1, 3, 9, 5, 2];
 let arr2 = [3, 2, 5, 7, 8];
 
-solution(arr1, arr2);
+console.log(solution(arr1, arr2));
