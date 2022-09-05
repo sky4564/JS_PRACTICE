ba12efd497086d8e3358eb791c18e20b1ee6aabb
hongsokan <hongsokan@daum.net>
2022-09-05 23:04:07 +0900

    sec06 update

--- ahs/sec06/ex06.js
+++ ahs/sec06/ex06.js
@@ -1,9 +1,16 @@
 // 공주 구하기
 
-function solution() {
-
+function solution(n, k) {
+    let answer;
+    let queue = Array.from({ length: n }, (v, i) => i + 1);
+    while (queue.length) {
+        for (let i = 1; i < k; i++) queue.push(queue.shift());
+        queue.shift();
+        if (queue.length === 1) answer = queue.shift();
+    }
+    return answer;
 }
 
 let n = 8;
 let k = 3;
-console.log(solution(n, k));
+console.log(solution(n, k)); // 7
