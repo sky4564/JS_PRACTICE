7a26a292391de442e57aa7d8496ba287fd484eef
hongsokan <hongsokan@daum.net>
2022-09-05 11:07:05 +0900

    sec06 update

---
+++ ahs/sec06/ex06.js
@@ -0,0 +1,9 @@
+// 공주 구하기
+
+function solution() {
+
+}
+
+let n = 8;
+let k = 3;
+console.log(solution(n, k));
---
+++ ahs/sec06/ex07.js
@@ -0,0 +1,10 @@
+// 교육과정 설계
+
+function solution() {
+
+}
+
+let required = "CBA";
+let designed = "CBDAGE";
+
+console.log(solution()); // YES
