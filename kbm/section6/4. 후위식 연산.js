70433fc811d26a8df6e78bea93aadcbc20d2ccdb
sky4564 <qw486512@gmail.com>
2022-09-06 18:01:48 +0900

    update

--- kbm/section6/4. 후위식 연산.js
+++ kbm/section6/4. 후위식 연산.js
@@ -1,4 +1,35 @@
 function solution(s){  
+stack = []
+  for(let x of s) {
+    let rt = 0
+    let lt = 0
+    if(!isNaN(x)) {
+      stack.push(Number(x))
+      // console.log(stack)
+    }
+    else if(x === '+') {
+      rt = stack.pop()
+      lt = stack.pop()
+      stack.push(lt+rt)
+    }
+    else if(x === '-') {
+      rt = stack.pop()
+      lt = stack.pop()
+      stack.push(lt-rt)
+    }
+    else if(x === '*') {
+      rt = stack.pop()
+      lt = stack.pop()
+      stack.push(lt*rt)
+    }
+    else if(x === '%') {
+      rt = stack.pop()
+      lt = stack.pop()
+      stack.push(lt/rt)
+    }    
+  }
+    ans = stack[0]
+  return ans
 }
 
 let str="352+*9-";
--- kbm/section6/5. 쇠막대기.js
+++ kbm/section6/5. 쇠막대기.js
@@ -1,5 +1,31 @@
 function solution(s){
+let pot = []
+let ans = 0;
+let flag = ''
+    for (let i = 0; i < a.length; i++ ){
+        if(s[i]=== '(') {
+            pot.push(s[i])
+            flag ='israz'    
+        }
+        else if (s[i]=== ')'){
+            if(s[i-1] ==='('){
+                if(flag === 'isbar'){
+                    pot.pop()
+                    ans += 1    
+                    break;
+                }
+                pot.pop(s[i])
+                ans += pot.length
+                flag = 'isbar'
+            }
+        }
+    }
+    return ans
 }
             
-let a="()(((()())(())()))(())";
+
+let a="()( ( ( ()() )(())()))(())";
+
+
+
 console.log(solution(a));
