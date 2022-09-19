10bdff11663c0ab7679ccd3f9934cc89a7f06446
hongsokan <hongsokan@daum.net>
2022-09-19 22:28:51 +0900

    sec05 update

--- ahs/sec05/ex04.js
+++ ahs/sec05/ex04.js
@@ -1,5 +1,6 @@
 // 연속 부분수열 2
 // 투포인터 알고리즘 참고
+// 포인터를 옮기면서 계속 수열이 5 이하인지 체크
 
 function solution(n, arr) {
     let answer = 0, lt = 0, sum = 0;
@@ -8,6 +9,7 @@ function solution(n, arr) {
         while (sum > n) {
             sum -= arr[lt++];
         }
+        // console.log("rt-lt+1=", rt-lt+1); // 1 2 3 2 2
         answer += (rt - lt + 1);
     }
     return answer;
