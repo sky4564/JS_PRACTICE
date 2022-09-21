c809d7c45c7120469ad8d6f73328acd34058ac30
hongsokan_mac <hongsokan@daum.net>
2022-09-21 11:14:04 +0900

    sec07 update

--- ahs/sec07/ex09.js
+++ ahs/sec07/ex09.js
@@ -1,11 +1,27 @@
 // 결혼식
 // 오는 시간, 가는 시간
 // 동시에 존재하는 최대 인원 구하기
+// 타임라인을 만들어 오는 시간 s 이면 ++, 가는 시간 e 이면 --
 
 function solution(times) {
-    let sorted = [...times].sort((a, b) => a[0] - b[0]);
-    console.log(sorted);
-
+    let answer = Number.MIN_SAFE_INTEGER;
+    let T_line = [];
+    for (let x of times) {
+        T_line.push([x[0], 's']);
+        T_line.push([x[1], 'e']);
+    }
+    T_line.sort((a, b) => {
+        if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+        else return a[0] - b[0];
+    });
+    console.log(T_line);
+    let cnt = 0;
+    for (let x of T_line) {
+        if (x[1] === 's') cnt++;
+        else cnt--;
+        answer = Math.max(answer, cnt);
+    }
+    return answer;
 }
 
 let input = [
---
+++ ahs/sec07/ex11.js
@@ -0,0 +1,10 @@
+// 뮤직비디오 (결정알고리즘)
+// DVD의 최소 용량 크기 출력
+
+function solution(input) {
+
+}
+
+let input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(input)); // 17
+// (1, 2, 3, 4, 5) (6, 7), (8, 9)
