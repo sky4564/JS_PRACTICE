471e0f93e9142241746d581cb4e0ab7d8e44648a
hongsokan_mac <hongsokan@daum.net>
2022-09-25 12:36:34 +0900

    sec08 update

--- ahs/sec08/ex02.js
+++ ahs/sec08/ex02.js
@@ -12,5 +12,19 @@ function solution(num) {
     convertToBinary(num);
 }
 
-let input = 15;
-console.log(solution(input));
+/*
+// 강의풀이
+function solution(n) {
+    let answer = "";
+    function DFS(n) {
+        if (n === 0) return;
+        else {
+            DFS(parseInt(n / 2));
+            answer += String(n % 2);
+        }
+    }
+    DFS(n);
+    return answer;
+}
+*/
+console.log(solution(11));
