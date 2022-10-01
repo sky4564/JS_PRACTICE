69a53ab6dd9063f9eb5ae0d2e50b4ebb76dd270b
hongsokan_mac <hongsokan@daum.net>
2022-10-01 15:58:20 +0900

    sec08 update

--- ahs/sec08/ex04.js
+++ ahs/sec08/ex04.js
@@ -4,6 +4,7 @@ function solution(n) {
   let answer = [];
   let ch = Array.from({ length: n + 1 }, () => 0);
   function DFS(L) {
+    console.log("L", L);
     if (L === n + 1) {
       let tmp = "";
       for (let i = 1; i <= n; i++) {
@@ -22,3 +23,4 @@ function solution(n) {
 }
 
 console.log(solution(3));
+// [ '1 2 3', '1 2', '1 3', '1', '2 3', '2', '3' ]
