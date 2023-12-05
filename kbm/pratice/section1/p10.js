3262dc4e811f331c7d77860226f82e2d2741606e
김병민 <kbm5553@gnifrix.com>
2023-12-05 11:36:07 +0900

    sec 1 문제  9 , 10 풀이

--- kbm/pratice/section1/p10.js
+++ kbm/pratice/section1/p10.js
@@ -1,4 +1,11 @@
             function solution(s, t){
+                let count = 0
+                for(x of s){                    
+                    if(x == t){
+                        count++
+                    }
+                }
+                return count
             }
             
             let str1="COMPUTERPROGRAMMING";
--- kbm/pratice/section1/p9.js
+++ kbm/pratice/section1/p9.js
@@ -1,5 +1,26 @@
-            function solution(s){
-            }
-            
-            let str="BANANA";
-            console.log(solution(str));
+
+// 문자열을 읽으면서 해당 문자열을 리턴
+// for(x of str) 써두 돼
+// function solution(s) {
+//     let ans = ''
+//     for (let i = 0; i < s.length; i++) {        
+//         console.log(s[i]);
+//         if (s[i] == "A") {
+//         ans += "x";
+//         } else ans += s[i];
+//     }
+//     return ans;
+// }
+
+
+//근데 대소문자 구분 등 정규표현식을 쓰면 
+// 좀더 문자열을 쉽게 변환 할 수 있음
+function solution(s) {
+    let ans = s
+    let regx = /A/gi
+    ans = ans.replace(regx,'#')
+    return ans
+}
+
+let str = "BANANAaaa";
+console.log(solution(str));
